a1e918df1f7360fbd237abe85d8e6e7d3f226cc1
Klittina <krisztike113@gmail.com>
2024-04-10 14:26:14 +0200

    regisztracio.php elkészítése, már be lehet jelentkezni, de még nem tárolja el a felhasználót, és pl a bejelentkezés sem megy

---
+++ php/regisztracio.php
@@ -0,0 +1,57 @@
+<?php
+  // a rendszer által ismert felhasználókat tartalmazó 2-dimenziós tömb
+  $fiokok = [
+    ["felhasznalonev" => "admin", "jelszo" => "admin123", "vezeteknev" => "Nagy", "keresztnev" => "Árpi", "email" => "admin@admin.com",]
+  ];
+  // űrlapfeldolgozás
+    $hibak = [];
+
+  if (isset($_POST["regisztral"])) {   // ha az űrlapot elküldték...
+    // a kötelezően kitöltendő mezők ellenőrzése
+    echo "<p>müködik</p>";
+    if (!isset($_POST["felhasznalonev"]) || trim($_POST["felhasznalonev"]) === "")
+      $hibak[] = "A felhasználónév megadása kötelező!";
+
+    if (!isset($_POST["jelszo"]) || trim($_POST["jelszo"]) === "" || !isset($_POST["jelszo2"]) || trim($_POST["jelszo2"]) === "")
+      $hibak[] = "A jelszó és az ellenőrző jelszó megadása kötelező!";
+
+    
+    // űrlapadatok lementése változókba
+    $felhasznalonev = $_POST["felhasznalonev"];
+    $jelszo = $_POST["jelszo"];
+    $jelszo2 = $_POST["jelszo2"];
+    $vezeteknev = $_POST["vezeteknev"];
+    $keresztnev = $_POST["keresztnev"];
+    $email = $_POST["email"];
+
+    // foglalt felhasználónév ellenőrzése
+    foreach ($fiokok as $fiok) {
+      if ($fiok["felhasznalonev"] === $felhasznalonev)  // ha egy regisztrált felhasználó neve megegyezik az űrlapon megadott névvel...
+        $hibak[] = "A felhasználónév már foglalt!";
+    }
+
+    // túl rövid jelszó
+    if (strlen($jelszo) < 5)
+      $hibak[] = "A jelszónak legalább 5 karakter hosszúnak kell lennie!";
+
+    // a két jelszó nem egyezik
+    if ($jelszo !== $jelszo2)
+      $hibak[] = "A jelszó és az ellenőrző jelszó nem egyezik!";
+
+      $siker = FALSE;
+    // regisztráció sikerességének ellenőrzése
+    if (count($hibak) === 0) {   // ha nem történt hiba a regisztráció során, hozzáadjuk az újonnan regisztrált felhasználót a $fiokok tömbhöz
+      $fiokok[] = ["felhasznalonev" => $felhasznalonev, "jelszo" => $jelszo, "keresztnev" => $keresztnev, "vezeteknev" => $vezeteknev, "email" => $email];
+      $siker = TRUE;
+    }
+    
+    if (isset($siker) && $siker === TRUE) {  // ha nem volt hiba, akkor a regisztráció sikeres
+        echo "<p>Sikeres regisztráció!</p>";
+        //header("Location: ../index.html"); //SIkeres bejelentkezés esetén továbbítja a felhasználót a főoldalra
+      } else {                                // az esetleges hibákat kiírjuk egy-egy bekezdésben
+        foreach ($hibak as $hiba) {
+          echo "<p>" . $hiba . "</p>";
+        }
+      }
+  }
+?>
